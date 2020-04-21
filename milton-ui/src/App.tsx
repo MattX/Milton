31b09efe5342b3c07f9935d8782d9955ae2a1827
Matthieu Felix <matthieufelix@gmail.com>
2020-04-20 22:32:11 -0400

    Fix UI on mobile

--- milton-ui/src/App.tsx
+++ milton-ui/src/App.tsx
@@ -10,6 +10,7 @@ interface AppState {
     selectedPostText: string | undefined,
     readerState: ReaderState,
     viewingSearch: boolean,
+    showingPostList: boolean,
 }
 
 class App extends React.Component<{}, AppState> {
@@ -21,6 +22,7 @@ class App extends React.Component<{}, AppState> {
             selectedPostText: undefined,
             readerState: ReaderState.NONE,
             viewingSearch: false,
+            showingPostList: true,
         }
     }
 
@@ -29,7 +31,7 @@ class App extends React.Component<{}, AppState> {
     }
 
     selectPost(post: Post): void {
-        this.setState({ selectedPost: post, readerState: ReaderState.LOADING })
+        this.setState({ selectedPost: post, readerState: ReaderState.LOADING, showingPostList: false })
         getPostContent(post.storageId).then((content) =>
             this.setState({selectedPostText: content, readerState: ReaderState.LOADED}))
     }
@@ -44,24 +46,32 @@ class App extends React.Component<{}, AppState> {
         listPosts().then((posts) => this.setState({posts: posts}));
     }
 
+    togglePostList(): void {
+        this.setState({showingPostList: !this.state.showingPostList});
+    }
+
     render() {
         const clearSearch = this.state.viewingSearch ? this.clearSearch.bind(this) : undefined;
         return (
-            <div className="App container-fluid">
-                <div className="row">
-                    <div className="col-md-4 postListContainer">
-                        <PostList posts={this.state.posts}
-                                  selectedUrl={this.state.selectedPost?.url}
-                                  selectPost={this.selectPost.bind(this)}
-                                  search={this.search.bind(this)}
-                                  clearSearch={clearSearch}/>
-                    </div>
-                    <div className="col-md-8 readerContainer">
-                        <Reader articleHtml={this.state.selectedPostText}
-                                readerState={this.state.readerState}
-                                articleUrl={this.state.selectedPost?.url}
-                                articleTitle={this.state.selectedPost?.title}/>
-                    </div>
+            <div className="App">
+                <header className="header">
+                    <button className="btn showPostListBtn" onClick={this.togglePostList.bind(this)}>☰</button>
+                    <h3 className="headerName">The Milton Library Assistant</h3>
+                </header>
+                <div className="mainContent">
+                <div className={"postListContainer" + (this.state.showingPostList ? " inScope" : "")}>
+                    <PostList posts={this.state.posts}
+                              selectedUrl={this.state.selectedPost?.url}
+                              selectPost={this.selectPost.bind(this)}
+                              search={this.search.bind(this)}
+                              clearSearch={clearSearch}/>
+                </div>
+                <div className="readerContainer">
+                    <Reader articleHtml={this.state.selectedPostText}
+                            readerState={this.state.readerState}
+                            articleUrl={this.state.selectedPost?.url}
+                            articleTitle={this.state.selectedPost?.title}/>
+                </div>
                 </div>
             </div>
         );
--- milton-ui/src/PostList.tsx
+++ milton-ui/src/PostList.tsx
@@ -28,7 +28,7 @@ const Listing = (props: PostListProps) => (
                 </span><br/>
                     {post.site === undefined ? "" : "from " + post.site}
                 </button>
-            </li>
+            </li>;
         })
     }</ul>
 );
@@ -36,46 +36,45 @@ const Listing = (props: PostListProps) => (
 export class PostList extends React.Component<PostListProps, SearchState> {
     constructor(props: PostListProps) {
         super(props);
-        this.state = {query: ''}
+        this.state = {query: ''};
     }
 
     updateQuery(event: any) {
-        this.setState({query: event.target.value})
+        this.setState({query: event.target.value});
     }
 
     render(): React.ReactNode {
         let inner;
         if (this.props.posts === undefined) {
-            inner = <Spinner/>
+            inner = <Spinner/>;
         } else {
-            inner = Listing(this.props)
+            inner = Listing(this.props);
         }
 
         let clearSearchBtn;
         if (this.props.clearSearch === undefined) {
-            clearSearchBtn = <React.Fragment/>
+            clearSearchBtn = <React.Fragment/>;
         } else {
             clearSearchBtn = <button className="btn btn-outline-danger clear-search" type="button"
                                      onClick={this.props.clearSearch}>
                 Clear search results
-            </button>
+            </button>;
         }
 
         return (
             <div className="postList">
-                <header className="postListHeader"><h3>The Milton Library Assistant</h3></header>
-                    <form onSubmit={(e) => {e.preventDefault(); this.props.search(this.state.query)}}>
-                        <div className="input-group">
-                        <input type="text" className="form-control" placeholder="Search"
-                           value={this.state.query} onChange={this.updateQuery.bind(this)}/>
-                        <div className="input-group-append" id="button-addon4">
-                            <button className="btn btn-outline-primary" type="button">Search</button>
-                        </div>
-                        </div>
-                    </form>
+                <form onSubmit={(e) => {e.preventDefault(); this.props.search(this.state.query);}}>
+                    <div className="input-group">
+                    <input type="text" className="form-control" placeholder="Search"
+                       value={this.state.query} onChange={this.updateQuery.bind(this)}/>
+                    <div className="input-group-append" id="button-addon4">
+                        <button className="btn btn-outline-primary" type="submit">Search</button>
+                    </div>
+                    </div>
+                </form>
                 {clearSearchBtn}
                 {inner}
             </div>
         );
     }
-};
+}
